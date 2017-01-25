45ac787406acc241f113aa54e1ef02bd3b99a3fe
Theo Richart <theo.richart@gmail.com>
2017-01-25 13:24:32 -0500

    set slide, integration test

--- src/GitShow/Library.fs
+++ src/GitShow/Library.fs
@@ -1,6 +1,7 @@
 namespace GitShow
 
 open Chessie.ErrorHandling
+open YamlDotNet.Serialization
 /// Documentation for my library
 ///
 /// ## Example
@@ -8,21 +9,47 @@ open Chessie.ErrorHandling
 ///     let h = Library.hello 1
 ///     printfn "%d" h
 ///
-module Library = 
+module Slide = 
+  open Process
   
 
-  type Slide = {
+  type T = {
     commit: string;
-    command: string
+    command: string option
   }
-  type GitError = ModifiedFiles
+  let from (id:string) : T = { commit=id; command=None }
+  type Error = Unknown | ModifiedFiles | Serialization of exn
   type IImpl =
-    abstract member Checkout: Slide -> Result<string, GitError>
-  type GitImpl =
+    abstract member SetSlide: T -> Result<string, Error>
+    abstract member GetCurrent: Unit -> Result<T, Error>
+
+  type GitImpl() =
+    let runf = ignore << Process.runProcess ignore ignore "git"
     interface IImpl with
-        override x.Checkout(s:Slide) = ok s.commit
+        override x.SetSlide(s:T) =
+          runf ["checkout"; "-q"; s.commit]
+          match s.command with
+          | _ -> ()
+          ok s.commit
+        override x.GetCurrent() = fail Unknown
         
-  type Presentation = Slide[]
+
+  type Presentation = T array
+
+  let load (path:string): Result<Presentation, Error> =
+    use f:System.IO.TextReader = upcast(System.IO.File.OpenText(path))
+    
+    let d = Deserializer()
+    Chessie.ErrorHandling.Trial.Catch (fun (x:System.IO.TextReader) -> d.Deserialize<Presentation>(x)) f
+    |> mapFailure (List.map Serialization)
+
+  let save (path:string) (p:Presentation): Result<unit, Error> =
+    use f:System.IO.TextWriter = upcast(new System.IO.StreamWriter(path))
+    
+    let d = Serializer()
+    Chessie.ErrorHandling.Trial.Catch (fun (x:System.IO.TextWriter) -> d.Serialize(x, p)) f
+    |> mapFailure (List.map Serialization)
+    
 
   /// Returns 42
   ///
--- tests/GitShow.Tests/Tests.fs
+++ tests/GitShow.Tests/Tests.fs
@@ -1,10 +1,61 @@
 module GitShow.Tests
 
-open GitShow
+open Chessie.ErrorHandling
+open GitShow.Process
 open NUnit.Framework
+open System.IO
+open GitShow.Slide
+
+let inline assertSuccess r = failed r |> Assert.False
+
+let runf args =
+    let mutable r = []
+    let p = Process.runProcess (fun x -> r <- x :: r) (printfn "ERROR: %s") "git" args
+    Seq.toArray r
+
+let testRepo () =
+    let rec setAttributes (di:DirectoryInfo) =
+        if di.Exists then
+            di.Attributes <- FileAttributes.Normal
+            di.GetFiles() |> Array.iter (fun f -> f.Attributes <- FileAttributes.Normal)
+            di.GetDirectories() |> Array.iter setAttributes
+    let path = Path.Combine [|(Path.GetFullPath(".")); "test"|]
+    path |> DirectoryInfo |> setAttributes
+    if Directory.Exists path then Directory.Delete(path, true)
+    Assert.DoesNotThrow (fun () -> ignore <| Directory.CreateDirectory(path))
+    printfn "Cur path %s" path
+    Directory.SetCurrentDirectory path
+
+    let log = ref false
+    let logIf label = (fun s -> if !log then printfn "%s %s" label s else ignore s)
+    let run = ignore << Process.runProcess (logIf "OUT") (logIf "ERROR") "git"
+    run ["init"]
+    File.WriteAllText("test1.txt", "line1")
+    run ["status"]
+    run ["add"; "."]
+    run ["commit"; "-amStep1"]
+    File.WriteAllText("test1.txt", "line1\nline2")
+    run ["commit"; "-amStep2"]
+    File.WriteAllText("test2.txt", "line1\nline2")
+    run ["add"; "."]
+    run ["commit"; "-amStep3"]
+    let commits = runf ["log"; "--pretty=format:\"%H\""]
+    printfn "Commits: %A" commits
+    let pres = commits |> Array.map Slide.from
+    printfn "Pres: %A" pres
+    Slide.save "pres.yml" pres |> assertSuccess
+    pres
 
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``setup repo`` () =
+    let p = testRepo()
+    let nthId n = (Array.item n p).commit
+    let r:IImpl = upcast GitImpl()
+    r.SetSlide (p.[0]) |> assertSuccess
+    let curCommit() = runf ["rev-parse"; "HEAD"] |> Array.item 0
+    printfn "Currently at: %A" (curCommit())
+    Assert.AreEqual(nthId 0, curCommit())
+    r.SetSlide (Array.last p) |> assertSuccess
+    Assert.AreEqual(nthId (Array.length p - 1), curCommit())
+//    r.
+    ()
